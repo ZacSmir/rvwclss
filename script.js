aceb6d437b70b79eb1e6583986d68b786c6edbf8
ZacSmir <Zach@smirin.us>
2016-10-14 08:52:11 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 var list = ["cat", "dog", "monkey", "elephant", "zebra", "beaver"]
 console.log()
 
-function pickAnimal(list){
-  var item = list[Math.floor(Math.random()*list.length)];
-  console.log(item)
-}
+//function pickAnimal(list){
+//  var item = list[Math.floor(Math.random()*list.length)];
+//  console.log(item)
+//}
 
 var otherList = ["fish", "Gorilla", "Mosquitoe", "Cow"]
 
-pickAnimal(otherList)
+//pickAnimal(otherList)
 
 function pickTwoAnimals(listOne, listTwo) {
   var doublelist = listOne.concat(listTwo)
-
+  var item = doublelist[Math.floor(Math.random()*doublelist.length)];
+  console.log(item)
 }
 
-pickTwoAnimals(list)
+pickTwoAnimals(list, otherList)
