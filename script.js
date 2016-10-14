7f5b2798a8467bd09f37fade98b82c9bc0293056
ZacSmir <Zach@smirin.us>
2016-10-14 09:24:50 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ console.log()
 //
 //var otherList = ["fish", "Gorilla", "Mosquitoe", "Cow"]
 //
-////pickAnimal(otherList) This works. I just commented it out bc too many things running at once
+//pickAnimal(otherList) This works. I just commented it out bc too many things running at once
 //
 //function pickTwoAnimals(listOne, listTwo) {
 //  var doublelist = listOne.concat(listTwo)
@@ -17,7 +17,7 @@ console.log()
 //  return item
 //}
 //
-////pickTwoAnimals(list, otherList) This works. I just commented it out bc too many things running at once
+//pickTwoAnimals(list, otherList) This works. I just commented it out bc too many things running at once
 //
 //function animalNameLong(name){
 //console.log(name.length)
@@ -37,6 +37,8 @@ for (i = 0; i < 10; i++) {
   console.log("hi");
 }
 
-
-if{}
+var item = list[Math.floor(Math.random()*list.length)];
+if(item = "dog"){
+console.log("FISH")
+}
 
