622dab6d1bc5ef6517a032ccc2b4bcca94952007
ZacSmir <Zach@smirin.us>
2016-10-14 09:27:53 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -18,27 +18,28 @@ console.log()
 //}
 //
 //pickTwoAnimals(list, otherList) This works. I just commented it out bc too many things running at once
-//
+
 //function animalNameLong(name){
 //console.log(name.length)
-////}
-//function doThings(){
-//things = 2 + 2
-//console.log(things)
 //}
-//
-//function doOtherThings(thingsagain){
-//otherThings = thingsagain + 4
-//}
-//
-//doOtherThings(doThings)
-//
-//for (i = 0; i < 10; i++) { 
-//  console.log("hi");
+function doThings(){
+things = 2 + 2
+console.log(things)
+}
+
+function doOtherThings(thingsagain){
+otherThings = thingsagain + 4
+}
+
+doOtherThings(doThings)
+
+for (i = 0; i < 10; i++) { 
+  console.log("hi");
+}
+
+//var item = list[Math.floor(Math.random()*list.length)];
+//if (item.length == 3) {
+//console.log("FISH")
 //}
 
-var item = list[Math.floor(Math.random()*list.length)];
-if (item.length = 3) {
-console.log("FISH")
-}
 
