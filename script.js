779dfb8fc5386e19a034631b0c69cb2e882b0fd4
ZacSmir <Zach@smirin.us>
2016-10-14 08:46:21 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 var list = ["cat", "dog", "monkey", "elephant", "zebra", "beaver"]
-console.log(list.length)
+console.log()
+var item = list[Math.floor(Math.random()*list.length)];
+console.log(item)
 function pickAnimal(){}
