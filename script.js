a58827e2bf9a94b778ca50e674bac7fb97d1bb06
ZacSmir <Zach@smirin.us>
2016-10-14 08:54:35 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 var list = ["cat", "dog", "monkey", "elephant", "zebra", "beaver"]
 console.log()
 
-//function pickAnimal(list){
-//  var item = list[Math.floor(Math.random()*list.length)];
-//  console.log(item)
-//} This works. I just commented it out bc too many things running at once
+function pickAnimal(list){
+  var item = list[Math.floor(Math.random()*list.length)];
+  console.log(item)
+} 
 
 var otherList = ["fish", "Gorilla", "Mosquitoe", "Cow"]
 
 //pickAnimal(otherList) This works. I just commented it out bc too many things running at once
 
-//function pickTwoAnimals(listOne, listTwo) {
-//  var doublelist = listOne.concat(listTwo)
-//  var item = doublelist[Math.floor(Math.random()*doublelist.length)];
-//  console.log(item)
-//} This works. I just commented it out bc too many things running at once
+function pickTwoAnimals(listOne, listTwo) {
+  var doublelist = listOne.concat(listTwo)
+  var item = doublelist[Math.floor(Math.random()*doublelist.length)];
+  console.log(item)
+}
 
 //pickTwoAnimals(list, otherList) This works. I just commented it out bc too many things running at once
 
-animalNameLong
+function animalNameLong(name){
+console.log(name.length)
+}
+
+animalNameLong()
